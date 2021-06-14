e95efec6fc0fd73468161a992ff623e9075724c1
Angga Pradipta <anggaa.pradipta@gmail.com>
2021-06-14 22:31:41 +0700

    devices

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({
 >) => {
   return (
     <button
-      className={`mt-8 align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none w-full px-4 py-2 rounded-lg text-white bg-indigo-600 active:bg-purple-600 hover:bg-indigo-700  ${
+      className={`align-bottom inline-flex items-center justify-center cursor-pointer leading-5 transition-colors duration-150 font-medium focus:outline-none px-4 py-2 rounded-lg text-white bg-indigo-600 active:bg-purple-600 hover:bg-indigo-700  ${
         props.disabled && "opacity-50"
       } ${className}`}
       {...props}
--- src/pages/Devices/index.tsx
+++ src/pages/Devices/index.tsx
@@ -1,12 +1,98 @@
 import React from "react";
 import { Card } from "../../components/Card";
+import { Disclosure, Transition, Switch } from "@headlessui/react";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
 
 export const Devices = () => {
   return (
-    <div className="grid grid-cols-3 gap-6">
-      <Card>eue</Card>
-      <Card>eue</Card>
-      <Card>eue</Card>
+    <div className="flex flex-col">
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="w-full focus:outline-none">
+              <Card className="flex gap-6">
+                <div
+                  className={`w-12 h-12 flex items-center justify-center rounded bg-lightBlue-100 self-center`}
+                >
+                  <i
+                    className={`fas fa-door-closed text-xl text-lightBlue-600`}
+                  />
+                </div>
+                <div className=" flex flex-col items-start">
+                  <span className="text-blueGray-400 text-xs mb-1">Room</span>
+                  <span className="text-lg font-bold">Living Room</span>
+                  <span className="text-blueGray-500">
+                    Current temperature is{" "}
+                    <span className="text-blueGray-800 font-medium">32 C</span>
+                  </span>
+                </div>
+
+                <div className="flex-1" />
+
+                <Button className="self-center text-xs">Set as Main</Button>
+
+                <div className="flex justify-end items-center">
+                  <i
+                    className={`${
+                      open ? "fa-chevron-up" : "fa-chevron-down"
+                    } fas text-purple-500`}
+                  />
+                </div>
+              </Card>
+            </Disclosure.Button>
+
+            <Transition
+              enter="transition duration-100 ease-out"
+              enterFrom="transform -translate-y-12 opacity-0"
+              enterTo="transform translate-y-0 opacity-100"
+              leave="transition  duration-75 ease-out"
+              leaveFrom="transform translate-y-0  opacity-100"
+              leaveTo="transform  -translate-y-12 opacity-0"
+            >
+              <Disclosure.Panel className="mt-1">
+                <Card className="flex flex-col gap-4">
+                  <Card className="flex gap-6 bg-blue-100 shadow-none">
+                    <div
+                      className={`w-12 h-12 flex items-center justify-center rounded bg-white self-center`}
+                    >
+                      <i
+                        className={`fas fa-door-closed text-xl text-lightBlue-600`}
+                      />
+                    </div>
+                    <div>
+                      <div className=" flex flex-col items-start">
+                        <span className="text-blueGray-500 text-xs mb-1">
+                          Device
+                        </span>
+                        <span className="text-lg font-bold">Lamp #1</span>
+                      </div>
+                    </div>
+
+                    <div className="flex-1" />
+                    <div>
+                      <Button className="self-center text-xs mr-2">Edit</Button>
+                      <Button className="self-center text-xs bg-red-400">
+                        Delete
+                      </Button>
+                    </div>
+                  </Card>
+                  <div className="bg-blueGray-200 flex justify-center items-center rounded-lg p-4">
+                    <i className="fas fa-plus text-xl text-blue-600 mr-4" />
+                    <div className="font-medium text-blueGray-600">
+                      Add device
+                    </div>
+                  </div>
+                </Card>
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+      <div className="bg-blueGray-200 flex justify-center items-center rounded-lg p-4 mt-6">
+        <i className="fas fa-plus text-xl text-blue-600 mr-4" />
+        <div className="font-medium text-blueGray-600">Add Room</div>
+      </div>
     </div>
   );
 };
